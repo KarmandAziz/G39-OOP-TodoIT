e2f11533f0742874ee94dab2b76ceb36153be3bc
KarmandAziz <Karmand94@gmail.com>
2021-11-16 11:56:44 +0100

    JSONManager added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.model.*;
 
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,16 +20,20 @@ public class App {
 
 
 
+    }
 
+    private static void copy(File source, File destination) {
+        try(InputStream in = new FileInputStream(source);
+            OutputStream out = new FileOutputStream(destination)){
 
-
-
-
-
-
-
-
-
-
+            int b;
+            while((b = in.read()) != -1){
+                out.write(b);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
